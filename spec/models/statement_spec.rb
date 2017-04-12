310241e3ff945d8bd0da3df51b90ac609427ca5d
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-04-12 15:22:16 +0200

    Refactored statement model tests

--- spec/models/statement_spec.rb
+++ spec/models/statement_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Statement, type: :model do
-  context 'validations' do
+  context '#title' do
     it { should validate_presence_of(:title) }
+  end
+
+  context '#text' do
     it { should validate_presence_of(:text) }
   end
 end
