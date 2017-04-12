8251a9e294735daa2e26db522b8cb01d1d291f15
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-04-12 16:35:05 +0200

    Added case insensitive uniqueness validation to stop name [#143584259]

--- spec/models/stop_spec.rb
+++ spec/models/stop_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Stop, type: :model do
   context '#name' do
     it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
   end
 end
