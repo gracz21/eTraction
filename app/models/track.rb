b9c6c7c77fb2381f2a5ef16d9855b57549f960cf
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-04-20 18:58:46 +0200

    Added presence validation of track stops [#143584259]

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,5 +2,6 @@ class Track < ApplicationRecord
   has_many :track_items
   has_many :stops, through: :track_items
 
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, :track_items, :stops, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
 end
