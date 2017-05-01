8c5e56b1477663ff52fab99e0aac5ed6911777de
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-05-01 17:02:58 +0200

    Added get user endpoint [#144276963]

--- app/controllers/api/v1/users.rb
+++ app/controllers/api/v1/users.rb
@@ -1,6 +1,27 @@
 module API::V1
   class Users < Grape::API
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      error!({errors: e.message}, 404)
+    end
+
     resource :users do
+      desc 'Return user based on given device identifier', {
+          success: { model: API::Entities::User },
+          failure: [ { code: 401, message: 'Unauthorized' },
+                     {code: 404, message: 'User not found'} ],
+          headers: {
+              'Device-Id' => {
+                  description: 'Android device identifier',
+                  required: true
+              }
+          }
+      }
+      get do
+        error!('Unauthorized', 401) if headers['Device-Id'].nil?
+
+        present User.find_by!(device_id: headers['Device-Id']), with: API::Entities::User
+      end
+
       desc 'Create new user or return user if already created', {
           success: { model: API::Entities::User },
           failure: [ { code: 401, message: 'Unauthorized' } ],
@@ -20,7 +41,7 @@ module API::V1
         error!('Unauthorized', 401) if headers['Device-Id'].nil?
 
         user = User.find_or_initialize_by(device_id: headers['Device-Id'])
-        user.username = params[:user][:username] unless user.username == params[:user][:username]
+        user.username = params[:user][:username]
         user.save
         error!({errors: user.errors}, 400) unless user.persisted?
         present user, with: API::Entities::User
--- spec/api/v1/users_spec.rb
+++ spec/api/v1/users_spec.rb
@@ -3,14 +3,49 @@ require 'rails_helper'
 describe API::V1::Users, type: :request do
   let(:user_entity) { API::Entities::User }
 
+  describe 'GET /api/v1/users' do
+    let!(:user) { FactoryGirl.create(:user) }
+
+    context 'when device identifier is included in request headers' do
+      context 'when device with given id is already registered' do
+        let!(:headers) { { 'Device-Id' => user.device_id } }
+
+        before { get '/api/v1/users', headers: headers }
+
+        it { expect(response).to have_http_status(200) }
+
+        it 'should return user record connected to given device id' do
+          expect(response.body).to eq user_entity.represent(user).to_json
+        end
+      end
+
+      context 'when device with given id is not registered' do
+        let!(:headers) { { 'Device-Id' => SecureRandom.uuid } }
+
+        before { get '/api/v1/users', headers: headers }
+
+        it { expect(response).to have_http_status(404) }
+      end
+    end
+
+    context 'when device identifier is included in request headers' do
+      before { get '/api/v1/users' }
+
+      it { expect(response).to have_http_status(401) }
+    end
+  end
+
   describe 'POST /api/v1/users' do
+    let!(:user_params) { { user: { username: Faker::Name.name } } }
+
     context 'with device identifier included in request headers' do
       context 'with valid params' do
         context 'when device is already registered' do
           let!(:user) { FactoryGirl.create(:user) }
           let!(:user_params) { { user: { username: user.username } } }
+          let!(:headers) { { 'Device-Id' => user.device_id } }
 
-          before { post '/api/v1/users', params: user_params, headers: { 'Device-Id' => user.device_id } }
+          before { post '/api/v1/users', params: user_params, headers: headers }
 
           it { expect(response).to have_http_status(201) }
 
@@ -20,10 +55,11 @@ describe API::V1::Users, type: :request do
         end
 
         context 'when device is not registered' do
-          let!(:user_params) { { user: { username: Faker::Name.name } } }
           let!(:unregistered_device_id) { SecureRandom.uuid }
+          let!(:user_params) { { user: { username: Faker::Name.name } } }
+          let!(:headers) { { 'Device-Id' => unregistered_device_id } }
 
-          before { post '/api/v1/users', params: user_params, headers: { 'Device-Id' => unregistered_device_id } }
+          before { post '/api/v1/users', params: user_params, headers: headers }
 
           it { expect(response).to have_http_status(201) }
 
@@ -53,14 +89,15 @@ describe API::V1::Users, type: :request do
   end
 
   describe 'PUT /api/v1/users' do
+    let!(:user_params) { { user: { username: Faker::Name.name } } }
+
     context 'with device identifier included in request headers' do
       context 'when device is already registered' do
         let!(:user) { FactoryGirl.create(:user) }
+        let!(:headers) { { 'Device-Id' => user.device_id } }
 
         context 'with valid params' do
-          let!(:user_params) { { user: { username: Faker::Name.name } } }
-
-          before { put '/api/v1/users', params: user_params, headers: { 'Device-Id' => user.device_id } }
+          before { put '/api/v1/users', params: user_params, headers: headers }
 
           it { expect(response).to have_http_status(200) }
 
@@ -74,15 +111,13 @@ describe API::V1::Users, type: :request do
           let!(:other_user) { FactoryGirl.create(:user) }
           let!(:wrong_user_params) { { user: { username: other_user.username } } }
 
-          before { put '/api/v1/users', params: wrong_user_params, headers: { 'Device-Id' => user.device_id } }
+          before { put '/api/v1/users', params: wrong_user_params, headers: headers }
 
           it { expect(response).to have_http_status(400) }
         end
       end
 
       context 'when device is not registered' do
-        let!(:user_params) { { user: { username: Faker::Name.name } } }
-
         before { put '/api/v1/users', params: user_params, headers: { 'Device-Id' => SecureRandom.uuid } }
 
         it { expect(response).to have_http_status(401) }
@@ -90,8 +125,6 @@ describe API::V1::Users, type: :request do
     end
 
     context 'without device identifier in request headers' do
-      let!(:user_params) { { user: { username: Faker::Name.name } } }
-
       before { put '/api/v1/users', params: user_params }
 
       it { expect(response).to have_http_status(401) }
