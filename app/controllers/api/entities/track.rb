0f0a295b1e06c422a6553cb1855aeac1221c0ec0
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-05-05 14:53:19 +0200

    Added total travel time to track endpoint response [#143584259]

--- app/controllers/api/entities/track.rb
+++ app/controllers/api/entities/track.rb
@@ -3,9 +3,18 @@ module API
     class Track < Grape::Entity
       root :tracks, :track
 
-      expose :id, documentation: { type: Integer, presence: true }
+      expose :id,
+             documentation: { type: Integer, presence: true }
+      expose :total_travel_time,
+             documentation: { type: Integer, desc: 'Estimated total track time (in minutes)', presence: true }
       expose :track_items, using: API::Entities::TrackItem,
              documentation: { type: Array, desc: 'Track stops array', is_array: true, presence: true }
+
+      private
+
+      def total_travel_time
+        object.track_items.pluck(:travel_time).sum
+      end
     end
   end
 end
