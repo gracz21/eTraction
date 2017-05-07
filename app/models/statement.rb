58073e7956ef8c15638da708ef76f2c3d4cd8149
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-05-07 21:40:55 +0200

    Added notification after statement creation [#143583041]

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -1,3 +1,13 @@
 class Statement < ApplicationRecord
   validates :title, :text, presence: true
+
+  after_create :send_notifications
+
+  private
+
+  def send_notifications
+    ActionCable.server.broadcast 'statement_notification_channel',
+                                 id: id,
+                                 title: title
+  end
 end
