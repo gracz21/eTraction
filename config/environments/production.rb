2542168422011b00eeb44028921833711486726d
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-05-07 22:06:29 +0200

    Turned off request forgery protection on production [#143583041]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://etraction.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = %w(http://0.0.0.0 https://0.0.0.0 'chrome-extension://pfdhoblngboilpfeibdedpjgfnlcodoo')
+  config.action_cable.disable_request_forgery_protection = true
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
