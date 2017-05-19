e280a8742125e89fbfafd4f794b5e5495a288e6b
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-05-19 16:07:33 +0200

    Customized track insertion and update in rails_admin [#145390317]

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,7 +1,18 @@
 class Track < ApplicationRecord
-  has_many :track_items
+  has_many :track_items, inverse_of: :track
   has_many :stops, through: :track_items
 
-  validates :name, presence: true
+  validates :name, :track_items, presence: true
   validates :name, uniqueness: { case_sensitive: false }
+
+  accepts_nested_attributes_for :track_items
+
+  rails_admin do
+    list do
+      exclude_fields :stops
+    end
+    edit do
+      exclude_fields :stops
+    end
+  end
 end
--- app/models/track_item.rb
+++ app/models/track_item.rb
@@ -2,8 +2,16 @@ class TrackItem < ApplicationRecord
   default_scope { order(:track_id, :sequence_number) }
 
   belongs_to :stop
-  belongs_to :track
+  belongs_to :track, inverse_of: :track_items
 
-  validates :sequence_number, :travel_time, :stop, :track, presence: true
+  validates :sequence_number, :travel_time, :stop, presence: true
   validates :sequence_number, uniqueness: { scope: :track_id }
+
+  rails_admin do
+    visible false
+  end
+
+  def name
+    stop.name
+  end
 end
