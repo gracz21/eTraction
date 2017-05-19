06a8175e3fe4d5b915a47c2e932f9bdc014ea3da
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-05-19 18:10:25 +0200

    Customized list and edit for stop model in rails_admin [#145390317]

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -3,4 +3,13 @@ class Stop < ApplicationRecord
   has_many :tracks, through: :track_items
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  rails_admin do
+    list do
+      exclude_fields :track_items, :tracks
+    end
+    edit do
+      exclude_fields :track_items, :tracks
+    end
+  end
 end
