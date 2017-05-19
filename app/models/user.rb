a09f33d2d8d02342ef488aceaefed6fd6ace0bde
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-05-19 15:25:09 +0200

    Hidden messages in users list in rails_admin, defined user name [#145390317]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   has_many :messages
 
   validates :device_id, :username, presence: true, uniqueness: true
+
+  rails_admin do
+    list do
+      exclude_fields :messages
+    end
+  end
+
+  def name
+    username
+  end
 end
