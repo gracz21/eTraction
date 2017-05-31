72cd4a936f9e98fd6941a9af69933cbdb25ee401
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-05-31 22:32:48 +0200

    Added Geocoder to stop model [#146405663]

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -4,12 +4,15 @@ class Stop < ApplicationRecord
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
+  geocoded_by :name
+  after_validation :geocode, if: ->(obj) { obj.name.present? and obj.name_changed? }
+
   rails_admin do
     list do
       exclude_fields :track_items, :tracks
     end
     edit do
-      exclude_fields :track_items, :tracks
+      exclude_fields :track_items, :tracks, :latitude, :longitude
     end
   end
 end
