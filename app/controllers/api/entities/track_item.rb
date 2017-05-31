78b4dcb2c03fd6a0ed0b924ee301bd15e1c16d41
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-05-31 22:33:21 +0200

    Added stop latitude and longitude to API response [#146405663]

--- app/controllers/api/entities/track_item.rb
+++ app/controllers/api/entities/track_item.rb
@@ -7,6 +7,10 @@ module API
              documentation: { type: Integer, desc: 'Stop sequence number in track', presence: true }
       expose :stop_name,
              documentation: { type: String, desc: 'Stop name', presence: true }
+      expose :stop_lat, as: :lat,
+             documentation: { type: Float, desc: 'Stop latitude', presence: true }
+      expose :stop_lon, as: :lon,
+             documentation: { type: Float, desc: 'Stop longitude', presence: true }
       expose :travel_time,
              documentation: { type: Integer, desc: 'Time needed to travel to this stop (in minutes)', presence: true }
 
@@ -14,6 +18,14 @@ module API
 
       def stop_name
         object.stop.name
+      end
+
+      def stop_lat
+        object.stop.latitude
+      end
+
+      def stop_lon
+        object.stop.longitude
       end
     end
   end
