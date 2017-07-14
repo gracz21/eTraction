c7905c88402fe89cd7c2211b8100996b4511b816
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-07-14 14:10:37 +0200

    Added uploaders for movies model [#148901057]

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ApplicationRecord
   validates :title, :genre, :length, :poster, :filename, presence: true
+
+  mount_uploader :poster, MoviePosterUploader
+  mount_uploader :filename, MovieFileUploader
 end
