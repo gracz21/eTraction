df747b1bd248bed934deaf6c1244ff2a95b59d47
Kamil Walkowiak <kamil.walek21@gmail.com>
2017-07-14 14:14:02 +0200

    Added uploader for restaurant menu items model [#148901057]

--- app/models/restaurant_menu_item.rb
+++ app/models/restaurant_menu_item.rb
@@ -2,4 +2,6 @@ class RestaurantMenuItem < ApplicationRecord
   validates :name, :price, :image, :weight, presence: true
   validates :name, uniqueness: { case_sensitive: false }
   validates :image, url: true
+
+  mount_uploader :image, RestaurantMenuItemImageUploader
 end
